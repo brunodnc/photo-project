1e12adcc99a85d3bd13c7c8c4c699f7fc6557eaf
brunodnc <brunocecchini@hotmail.com>
2022-12-30 02:04:48 -0300

    (api) [REFACTOR] Update classes to entities

--- api/src/main/java/jpb/photoproject/classes/Photo.java
+++ api/src/main/java/jpb/photoproject/classes/Photo.java
@@ -1,26 +1,100 @@
 package jpb.photoproject.classes;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 
+@Entity
+@Table(name = "photo")
 public class Photo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String file_name;
-    private String path;
-    private Boolean watermark;
-    private Boolean print;
-    private Boolean downloaded;
-    private Date created_at;
-    private Long tab;
 
-    public Photo(Long id, String file_name, String path, Boolean watermark, Boolean print, Boolean downloaded, Date created_at, Long tab) {
-        this.id = id;
-        this.file_name = file_name;
-        this.watermark = watermark;
-        this.print = print;
-        this.downloaded = downloaded;
-        this.created_at = created_at;
-        this.tab = tab;
+    @Column(name = "file_name")
+    private String fileName;
+
+    @Column(name = "path")
+    private String path;
+
+    @Column(name = "watermark")
+    private Boolean watermark;
+
+    @Column(name = "print")
+    private Boolean print;
+
+    @Column(name = "download")
+    private Boolean downloaded;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = Tab.class)
+    @JoinColumn(name = "tab", referencedColumnName = "id")
+    private Tab tab;
+
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Boolean getWatermark() {
+        return watermark;
+    }
+
+    public void setWatermark(Boolean watermark) {
+        this.watermark = watermark;
+    }
+
+    public Boolean getPrint() {
+        return print;
+    }
+
+    public void setPrint(Boolean print) {
+        this.print = print;
+    }
+
+    public Boolean getDownloaded() {
+        return downloaded;
+    }
+
+    public void setDownloaded(Boolean downloaded) {
+        this.downloaded = downloaded;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Tab getTab() {
+        return tab;
+    }
+
+    public void setTab(Tab tab) {
+        this.tab = tab;
+    }
 }
--- api/src/main/java/jpb/photoproject/classes/Photographer.java
+++ api/src/main/java/jpb/photoproject/classes/Photographer.java
@@ -1,24 +1,89 @@
 package jpb.photoproject.classes;
 
+import jakarta.persistence.*;
+
 import java.util.ArrayList;
 
+@Entity
+@Table(name = "photographer")
 public class Photographer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @OneToMany(fetch = FetchType.LAZY, targetEntity = Client.class, mappedBy = "client")
     private ArrayList<Client> clients;
+
+    @OneToOne(fetch = FetchType.LAZY, targetEntity = Subscription.class)
+    @JoinColumn(name = "subscription", referencedColumnName = "id")
     private Subscription subscription;
 
-    public Photographer(Long id, String name, String username, String email, String password, ArrayList<Client> clients, Subscription subscription) {
-        this.id = id;
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.clients = clients;
-        this.subscription = subscription;
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(ArrayList<Client> clients) {
+        this.clients = clients;
+    }
+
+    public Subscription getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(Subscription subscription) {
+        this.subscription = subscription;
+    }
 }
