ccc6dbe3332148561003e0c4b5e5345a52bb482f
joaopmi <joaopauloizar@hotmail.com>
2023-01-04 15:45:09 -0300

    (app) chore: initial steps of login view

--- app/src/app/login/login.component.ts
+++ app/src/app/login/login.component.ts
@@ -1,20 +1,16 @@
 import { Component, OnInit } from "@angular/core";
-import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from "@angular/forms";
-import { Observable, of } from "rxjs";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { ServicoUm } from "../services/servico1";
 
 @Component({
   selector: "login",
   templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"],
-  providers: [ServicoUm],
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
   /**string */
   public usernameFC = "username";
   public passwordFC = "password";
-  /**array */
-  private _usuariosJaExistentes = ["brunodnc", "balthus", "jaoKun", "coxa"];
   /**objeto */
   public atributos = {
     forca: 0,
@@ -23,80 +19,16 @@ export class LoginComponent implements OnInit {
 
   /**angular */
   public loginForm = new FormGroup({
-    [this.usernameFC]: new FormControl(
-      null,
-      [Validators.required],
-      [this.registeredUserCheck.bind(this)]
-      /* [this.funcaoAsync()] */
-    ),
+    [this.usernameFC]: new FormControl(null, [Validators.required], []),
     [this.passwordFC]: new FormControl(null, [Validators.required, Validators.minLength(8)], []),
   });
 
-  constructor(private _servico: ServicoUm) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    console.log(this._servico.contador += 10);
-    // exemplo de pipe e validação asyncrona
-    //   this.loginForm
-    //     .get(this.usernameFC)
-    //     .valueChanges.pipe(
-    //       filter(() => {
-    //         return this.loginForm.get(this.usernameFC).valid;
-    //       }),
-    //       map((valor: any) => {
-    //         return valor + " transformado";
-    //       })
-    //       tap((valor:any)=>{}),
-    //       debounceTime(2000)
-    //     )
-    //     .subscribe((valor: any) => {
-    //       console.log(valor);
-    //     });
-    // }
-    // funcaoAsync(): AsyncValidatorFn {
-    //   return (control: AbstractControl): Observable<ValidationErrors | null> => {
-    //     return null;
-    //   };
-    // }
-    // validacaoUsuarioAssincrono(control: AbstractControl): Observable<ValidationErrors | null> {
-    //   const valorInput = control.value;
-    //   let usuarioJaExiste: boolean;
-    //   if (this._usuariosJaExistentes.includes(valorInput)) {
-    //     usuarioJaExiste = true;
-    //   }
-    //   return usuarioJaExiste ? of({ jaExiste: true }) : of(null);
-  }
-
-  // fora do onInit ??
-  registeredUserCheck(control: AbstractControl): Observable<ValidationErrors | null> {
-    const { value } = control;
-    let registeredUsernames: string[]; // get registered usernames
-    return registeredUsernames.includes(value) ? of({ registered: true }) : of({ unregistered: true });
-  }
-
-  // acho que não está sendo usado esse get.
-  // get username() {
-  //   return this.loginForm.get("name");
-  // }
-
-  // get password() {
-  //   return this.loginForm.get("password");
-  // }
+  ngOnInit(): void {}
 
   login() {
     if (this.loginForm.invalid) return;
     const { username, password } = this.loginForm.value;
-    // const token = validateLogin();
-    // save token as dependency?
-    // route to home when login operations are done
   }
-
-  /* 
-  loginFormsModule(form:NgForm):void{
-    console.log(form.value);
-  } */
-
-  // loginFormsModule(): void {
-  //   console.log(this.atributos);
-  // }
 }
--- app/src/app/login/login.module.ts
+++ app/src/app/login/login.module.ts
@@ -2,16 +2,14 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
-import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { RouterModule } from "@angular/router";
-import { InputLoginComponent } from "./input-login";
 
 import { LoginComponent } from "./login.component";
 
 @NgModule({
-  declarations: [LoginComponent, InputLoginComponent],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
     RouterModule,
