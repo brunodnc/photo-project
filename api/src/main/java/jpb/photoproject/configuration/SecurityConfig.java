727940965f969d4668d38220bb541cb315d5c67d
brunodnc <brunocecchini@hotmail.com>
2023-04-06 16:37:10 -0300

    chore: trying to add keycloak

---
+++ api/src/main/java/jpb/photoproject/configuration/SecurityConfig.java
@@ -0,0 +1,38 @@
+package jpb.photoproject.configuration;
+
+@KeycloakConfiguration
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter
+{
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(buildSessionRegistry());
+    }
+
+    @Bean
+    protected SessionRegistry buildSessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        super.configure(http);
+        http
+                .authorizeRequests()
+                .antMatchers("/customers*").hasRole("USER")
+                .antMatchers("/admin*").hasRole("ADMIN")
+                .anyRequest().permitAll();
+    }
+}
