e44920710fa9931ac00a5c980b62dc24ae051df4
brunodnc <brunocecchini@hotmail.com>
2023-04-06 18:52:52 -0300

    fix: trying to update angular

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
+import { APP_INITIALIZER, NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MAT_DATE_FORMATS, MAT_DATE_LOCALE, NativeDateModule } from "@angular/material/core";
 import { MatDatepickerModule } from "@angular/material/datepicker";
@@ -8,6 +8,8 @@ import { MatInputModule } from "@angular/material/input";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { KeycloakAngularModule, KeycloakService } from "keycloak-angular";
+import { initializer } from "src/app-init";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MudaHtml } from "./directive/muda-html";
@@ -19,10 +21,18 @@ import { ProfileModule } from "./profile/profile.module";
 import { RegisterModule } from "./register/register.module";
 
 @NgModule({
-  declarations: [AppComponent,SafePipe,MudaHtml],
+  declarations: [AppComponent,
+    SafePipe,
+    MudaHtml
+   //kc
+    // AccessDeniedComponent,
+    // ManagerComponent,
+    // AdminComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    KeycloakAngularModule,
     LoginModule,
     RegisterModule,
     HomeModule,
@@ -36,12 +46,15 @@ import { RegisterModule } from "./register/register.module";
     HeaderModule,
     ProfileModule,
     HttpClientModule,
+   
   ],
   bootstrap: [AppComponent],
   providers: [
     {
-      provide: "TESTE",
-      useFactory: () => {},
+      provide: APP_INITIALIZER,
+      useFactory: initializer,
+      deps: [KeycloakService],
+      multi: true,
     },
     { provide: MAT_DATE_LOCALE, useValue: "pt-BR" },
     {
